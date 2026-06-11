2567d4570775fd50077259814a42447c3e618b15
lucasalbuquerque09 <lc.albuquerque09@gmail.com>
2026-06-11 17:11:39 -0300

    refatorar funções de exibição e limpeza no script.js

--- script.js
+++ script.js
@@ -4,24 +4,33 @@ let result = document.querySelector("#display");
 function addDisplay(valor) {
     // Verificar se o valor é um ponto decimal e se o display já contém um ponto
     if(result.value === "0" && valor === ".") {
+        
         return result.value += valor;
+    
     } else if(result.value === "" && valor === ".") {
-        return result.value = "0."
-    } else if(result.value === "ERRO") {
+       
+        return result.value = "0.";
+    
+    } else if(result.value === "ERRO" || result.value === "0") {
+        
         return result.value = valor;
-    }
+    };
     // Adicionar o valor ao display
     return result.value += valor;
 };
 
 // Função para limpar o display
 function cleanResult() {
+    
     return result.value = "";
+
 };
 
 // Função para deletar o último valor do display
 function deleteLastValue() {
-   return result.value = result.value.slice(0, -1);
+   
+    return result.value = result.value.slice(0, -1);
+
 };
 
 // Função para calcular o resultado da expressão no display
@@ -30,8 +39,11 @@ function calculateResult() {
     result.value === "" ? result.value = "ERRO" : result.value = result.value;
     // Tentar avaliar a expressão usando eval e exibir o resultado, ou exibir "ERRO" se houver um erro na avaliação
     try{
+       
         return result.value = eval(result.value);
+    
     } catch(erro){
+        
         return result.value = "ERRO";
     }
 };
